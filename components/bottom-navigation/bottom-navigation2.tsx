09fe990a477c7d98fedcdf43e29cac249264315c
presta <event@gmail.com>
2024-11-17 06:39:45 +0100

    test Fullscreen

--- components/bottom-navigation/bottom-navigation2.tsx
+++ components/bottom-navigation/bottom-navigation2.tsx
@@ -26,33 +26,65 @@ const BottomNavigation = () => {
 
   // Pour une ouverture en plein écran de l'application
     const enterFullscreen = () =>{
+      // const element = document.documentElement; // Ou un autre élément spécifique
+      // if (element.requestFullscreen) {
+      //   element.requestFullscreen();
+      // } else if (element.mozRequestFullScreen) { // Firefox
+      //   element.mozRequestFullScreen();
+      // } else if (element.webkitRequestFullscreen) { // Chrome, Safari, etc.
+      //   element.webkitRequestFullscreen();
+      // } else if (element.msRequestFullscreen) { // IE/Edge
+      //   element.msRequestFullscreen();
+      // }
+
       const element = document.documentElement; // Ou un autre élément spécifique
       if (element.requestFullscreen) {
         element.requestFullscreen(); // Navigateur moderne
-    } else if ((element as any).webkitRequestFullscreen) {
+      } else if ((element as any).webkitRequestFullscreen) {
         // Safari ou anciens navigateurs basés sur WebKit
         (element as any).webkitRequestFullscreen();
-    } else if ((element as any).msRequestFullscreen) {
+      } else if ((element as any).msRequestFullscreen) {
         // Anciennes versions d'Internet Explorer/Edge
         (element as any).msRequestFullscreen();
-    } else if ((element as any).mozRequestFullScreen) {
+      } else if ((element as any).mozRequestFullScreen) {
         // Ancienne implémentation de Firefox
         (element as any).mozRequestFullScreen();
-    } else {
-        console.warn("Le mode plein écran n'est pas pris en charge par ce navigateur.");
-    }
+      } else {
+        console.warn(
+          "Le mode plein écran n'est pas pris en charge par ce navigateur."
+        );
+      }
+
     }
 
     const exitFullscreen = () => {
+      // if (document.exitFullscreen) {
+      //   document.exitFullscreen();
+      // } else if (document.mozCancelFullScreen) { // Firefox
+      //   document.mozCancelFullScreen();
+      // } else if (document.webkitExitFullscreen) { // Chrome, Safari
+      //   document.webkitExitFullscreen();
+      // } else if (document.msExitFullscreen) { // IE/Edge
+      //   document.msExitFullscreen();
+      // }
+
       if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) { // Firefox
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) { // Chrome, Safari
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) { // IE/Edge
-        document.msExitFullscreen();
-      }
+        document.exitFullscreen().catch((error) => {
+            console.error("Erreur lors de la sortie du mode plein écran :", error);
+        });
+    } else if ((document as any).webkitExitFullscreen) {
+        // Safari ou anciens navigateurs basés sur WebKit
+        (document as any).webkitExitFullscreen();
+    } else if ((document as any).msExitFullscreen) {
+        // Anciennes versions d'Internet Explorer/Edge
+        (document as any).msExitFullscreen();
+    } else if ((document as any).mozCancelFullScreen) {
+        // Ancienne implémentation de Firefox
+        (document as any).mozCancelFullScreen();
+    } else {
+        console.warn("Le mode plein écran n'est pas pris en charge par ce navigateur.");
+    }
+
     }  
 
   return (
