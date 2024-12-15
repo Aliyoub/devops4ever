2a20f76fc003240918067c0c411c3c2ed41b5d37
presta <event@gmail.com>
2024-12-15 02:59:56 +0100

    parameters being added...

--- app/quiz/kubernetes/Services/page.tsx
+++ app/quiz/kubernetes/Services/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../../../store/store";
-import QuizPage from "../../quizPage";
+import QuizPage from "./QuizPage";
 import { questions } from "./questionServices";
 
 
