3ac833b8c573ed508ab3e5aeebdff3237dd31d06
presta <event@gmail.com>
2024-12-15 03:49:42 +0100

    parameters being added...

--- app/quiz/QuizQuestions.tsx
+++ app/quiz/QuizQuestions.tsx
@@ -22,8 +22,8 @@ const roboto = Roboto({
 const CustomRadio = styled(Radio)({
   "& .MuiSvgIcon-root": { fontSize: 15 },
   color: "rgb(224, 247, 250)",
-  //   color: "#3B8FEF",
-  fontWeight: "bolder",
+//   color: "#3B8FEF",
+  fontWeight:"bolder",
   "&.Mui-checked": { color: "#FCA4F0" },
 });
 
@@ -49,80 +49,60 @@ const QuizQuestions: React.FC<QuizQuestionsProps> = ({
   isLastQuestion,
 }) => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        maxHeight: "70vh",
-        overflowY: "auto",
-        marginBottom: "16px",
-      }}
-    >
-      <Typography className={(roboto.className, "questionText")} variant="h6">
-        {question}
-      </Typography>
-      <FormControl style={{ width: "100%" }} component="fieldset">
-        <RadioGroup value={currentSelection} onChange={onOptionChange}>
-          {Object.entries(options).map(([key, value], index) => (
-            <FormControlLabel
-              key={index}
-              value={index}
-              control={<CustomRadio />}
-              label={
-                <Typography
-                  sx={{ color: "rgb(224, 247, 250)", fontSize: "1rem" }}
-                >
-                  {value}
-                </Typography>
-              }
-              style={{
-                backgroundColor: "#3B8FEF",
-                borderBottom: "0.5px solid #FCA4F0",
-                padding: 0,
-                margin: 0,
-                width: "100%",
-              }}
-            />
-          ))}
-        </RadioGroup>
-      </FormControl>
+      <Box
+        sx={{
+          width: "100%",
+          maxHeight: "70vh",
+          overflowY: "auto",
+          marginBottom: "16px",
+        }}
+      >
+        <Typography  className={(roboto.className, "questionText")} variant="h6">
+          {question}
+        </Typography>
+        <FormControl style={{ width: "100%" }} component="fieldset">
+          <RadioGroup value={currentSelection} onChange={onOptionChange}>
+            {Object.entries(options).map(([key, value], index) => (
+              <FormControlLabel
+                key={index}
+                value={index}
+                control={<CustomRadio />}
+                label={
+                  <Typography sx={{ color: "rgb(224, 247, 250)", fontSize: "1rem" }}>
+                    {value}
+                  </Typography>
+                }
+                style={{
+                  backgroundColor: "#3B8FEF",
+                  borderBottom: "0.5px solid #FCA4F0",
+                  padding: 0,
+                  margin: 0,
+                  width: "100%",
+                }}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
       <Box display="flex" justifyContent="space-between" mt={3}>
         <Button
           variant="outlined"
           onClick={onPrevious}
           disabled={disablePrevious}
           className="previewButton"
-          sx={{ marginLeft: 7 }}
+          sx={{marginLeft: 2}}
         >
           Précédent
         </Button>
         <Button
           className={!isLastQuestion ? "nextButton" : ""}
-          variant="contained"
+          variant="outlined"
           onClick={onNext}
-          sx={{
-            marginRight: 7,
-            backgroundColor: "#FF5722", // Couleur de fond
-            color: "#fff", // Couleur du texte
-            fontWeight: "bold",
-            padding: "10px 20px",
-            borderRadius: "8px",
-            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.3)", // Effet d'ombre
-            transition: "all 0.2s ease-in-out", // Transition pour les effets interactifs
-            "&:hover": {
-              backgroundColor: "#FF7043", // Couleur de survol
-              transform: "translateY(-3px)", // Légère élévation sur survol
-              boxShadow: "0px 6px 8px rgba(0, 0, 0, 0.4)", // Ombre renforcée sur survol
-            },
-            "&:active": {
-              transform: "translateY(3px)", // Enfoncement lorsque cliqué
-              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)", // Ombre réduite
-            },
-          }}
+          sx={{marginRight: 2, borderWidth: 2}}
         >
           {isLastQuestion ? "Voir le score" : "Suivant"}
         </Button>
       </Box>
-    </Box>
+      </Box>
   );
 };
 
