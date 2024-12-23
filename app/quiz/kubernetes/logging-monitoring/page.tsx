394a8ee34511dad7eb55b6454a364413832a4b43
presta <event@gmail.com>
2024-12-23 10:27:21 +0100

    storage lecture added...

--- app/quiz/kubernetes/logging-monitoring/page.tsx
+++ app/quiz/kubernetes/logging-monitoring/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../../../store/store";
-import QuizPage from "./quizPage";
+import QuizPage from "../../quizPage/quizPage";
 import { questions } from "./questionsLoggingMonitoring";
 
 const Kubernetes_LoggingMonitoring: React.FC = () => {
