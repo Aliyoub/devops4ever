3ffbf53f4946adfe145e02173e3810b4676fdf5a
presta <event@gmail.com>
2025-01-05 21:31:15 +0100

    first commit 2025

--- app/quiz/kubernetes/listOfKubernetesThemes.tsx
+++ app/quiz/kubernetes/listOfKubernetesThemes.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import Kubernetes_ClusterArchitecture from './cluster-architecture/page'
-import Kubernetes_Security from './security/page'
-import Kubernetes_Services from './Services/page'
-import Kubernetes_Storage from './storage/page'
-import Kubernetes_Networking from './networking/page'
-import Kubernetes_Troubleshooting from './troubleshooting/page'
-import Kubernetes_InstallationConfiguration from './installation-configuration/page'
-import Kubernetes_WorkloadsScheduling from './workloads-scheduling/page'
-import Kubernetes_LoggingMonitoring from './logging-monitoring/page'
+import Kubernetes_ClusterArchitecture from "./cluster-architecture/page";
+import Kubernetes_Security from "./security/page";
+// import Kubernetes_Services from './Services/page'
+import Kubernetes_Storage from "./storage/page";
+import Kubernetes_Networking from "./networking/page";
+import Kubernetes_Troubleshooting from "./troubleshooting/page";
+import Kubernetes_InstallationConfiguration from "./installation-configuration/page";
+import Kubernetes_WorkloadsScheduling from "./workloads-scheduling/page";
+import Kubernetes_LoggingMonitoring from "./logging-monitoring/page";
 
 const ListOfKubernetesThemes = () => {
   return (
     <>
-    <Kubernetes_Security />
-    <Kubernetes_InstallationConfiguration />
-    <Kubernetes_WorkloadsScheduling />
-    <Kubernetes_LoggingMonitoring />
-    <Kubernetes_Storage />
-    <Kubernetes_Services />
-    <Kubernetes_Troubleshooting />
-    <Kubernetes_Networking />
-    <Kubernetes_ClusterArchitecture />
+      <Kubernetes_Security />
+      <Kubernetes_InstallationConfiguration />
+      <Kubernetes_WorkloadsScheduling />
+      <Kubernetes_LoggingMonitoring />
+      <Kubernetes_Storage />
+      {/* <Kubernetes_Services /> */}
+      <Kubernetes_Troubleshooting />
+      <Kubernetes_Networking />
+      <Kubernetes_ClusterArchitecture />
     </>
-  )
-}
+  );
+};
 
-export default ListOfKubernetesThemes
+export default ListOfKubernetesThemes;
