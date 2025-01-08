4857414349f9948c0b4eb270cf9f6b831868a855
presta <event@gmail.com>
2025-01-08 17:20:02 +0100

    bug in score component fixed

--- app/quiz/kubernetes/storage/page.tsx
+++ app/quiz/kubernetes/storage/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../../../store/store";
-import Quizpage from "../../quizpage/page";
+import Quizpage from "../../quizpage/quizpage";
 
 import { questions } from "./questionsStorage";
 
