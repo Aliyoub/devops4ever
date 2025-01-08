984c60d492422a5631c9dcebeb92242d09180f75
presta <event@gmail.com>
2025-01-08 06:43:37 +0100

    bug in score component fixed

--- app/quiz/kubernetes/Services/page.tsx
+++ app/quiz/kubernetes/Services/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../../../store/store";
-import QuizPage from "../../quizPage/QuizPage";
+import QuizPage from "./quizpage/QuizPage";
 import { questions } from "./questionsServices";
 
 const Kubernetes_Services: React.FC = () => {
--- app/quiz/kubernetes/networking/page.tsx
+++ app/quiz/kubernetes/networking/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../../../store/store";
-import QuizPage from "../../quizPage/QuizPage";
+import QuizPage from "./quizpage/QuizPage";
 
 import { questions } from "./questionsNetworking";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
